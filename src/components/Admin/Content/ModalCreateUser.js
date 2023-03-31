572f8d63c1d03fd4fe38c3f75f728b2fb96c2a8e
Tran Khac <trankhac05012002@gmail.com>
2023-03-31 11:16:35 +0700

    video 69.59

--- src/components/Admin/Content/ModalCreateUser.js
+++ src/components/Admin/Content/ModalCreateUser.js
@@ -1,8 +1,11 @@
+import { isValidDateValue } from '@testing-library/user-event/dist/utils';
 import axios from 'axios';
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { FcPlus } from 'react-icons/fc';
+import { toast } from 'react-toastify';
+
 
 const ModalCreateUser = (props) => {
   const { show, setShow } = props;
@@ -30,19 +33,29 @@ const ModalCreateUser = (props) => {
       // setPreviewImage("");
     }
   }
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
   const handleSubmitCreateUser = async () => {
     //validate
+    const IsValidEmail = validateEmail(email);
 
-    //call apis
-    //let data = {
-    //email: email,
-    //password: password,
-    //username: username,
-    //role: role,
-    //userImage: image
-    //}
-    //console.log(data)
+    if (!IsValidEmail) {
+      toast.error('invalid email')
+      //toast.success('test success')
+      //toast.info('test information')
+      return;
+    }
+    if (!password) {
+      toast.error('invalid invalid password')
+      return;
+    }
 
+    //submit data 
     const data = new FormData();
     data.append('email', email);
     data.append('password', password);
@@ -51,7 +64,14 @@ const ModalCreateUser = (props) => {
     data.append('userImage', image);
 
     let res = await axios.post('https://localhost:8081/api/v1/participant.com', data);
-    console.log(">>> check res:", res)
+    if (res.data && res.data.EC === 0) {
+      toast.success(res.data.EM);
+      handleClose();
+    }
+    if (res.data && res.data.EC !== 0) {
+      toast.success(res.data.EM);
+
+    }
   }
 
   //Design Model heading 64
